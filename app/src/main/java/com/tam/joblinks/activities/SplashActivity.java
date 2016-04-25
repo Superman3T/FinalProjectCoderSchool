05fe89cef164a7290d886511302d6e7a041ea93e
lasthulk <danchithancongab@gmail.com>
2016-04-25 20:10:41 +0700

    design splash screen

--- app/src/main/java/com/tam/joblinks/activities/SplashActivity.java
+++ app/src/main/java/com/tam/joblinks/activities/SplashActivity.java
@@ -1,11 +1,18 @@
 package com.tam.joblinks.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import com.tam.joblinks.R;
 import com.tam.joblinks.interfaces.UserRepositoryInterface;
 import com.tam.joblinks.repositories.UserRepository;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,5 +25,54 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        bindLogo();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // go to the main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        // Show splash screen for 3 seconds
+        new Timer().schedule(task, 2000);
+    }
+
+
+    private void bindLogo(){
+        // Start animating the image
+        final ImageView splash = (ImageView) findViewById(R.id.splash);
+        final AlphaAnimation animation1 = new AlphaAnimation(0.2f, 1.0f);
+        animation1.setDuration(700);
+        final AlphaAnimation animation2 = new AlphaAnimation(1.0f, 0.2f);
+        animation2.setDuration(700);
+        //animation1 AnimationListener
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation2 when animation1 ends (continue)
+                splash.startAnimation(animation2);
+            }
+            @Override
+            public void onAnimationRepeat(Animation arg0) {}
+            @Override
+            public void onAnimationStart(Animation arg0) {}
+        });
+
+        //animation2 AnimationListener
+        animation2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation1 when animation2 ends (repeat)
+                splash.startAnimation(animation1);
+            }
+            @Override
+            public void onAnimationRepeat(Animation arg0) {}
+            @Override
+            public void onAnimationStart(Animation arg0) {}
+        });
+
+        splash.startAnimation(animation1);
     }
 }
